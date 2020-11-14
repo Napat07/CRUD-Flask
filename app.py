b054c8f1098876b1e248252f39b60f9cb7a40824
Napat07 <naphat1811@gmail.com>
2020-11-14 22:18:14 +0700

    GET and POST

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, redirect, url_for, render_template
+from student import Student
+
+app = Flask(__name__)
+
+students = []
+
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
+
+@app.route('/students', methods=["GET", "POST"])
+def index():
+    if request.method == 'POST':
+        new_student = Student(request.form['first_name'], request.form['last_name'])
+        students.append(new_student)
+        return redirect(url_for('index'))
+    return render_template('index.html', students = students)
+
+@app.route('/students/new' ,methods=["GET", "POST"])
+def new():
+    return  render_template('new.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
